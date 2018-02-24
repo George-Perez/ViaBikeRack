982830791f1565d1b8e4d70693a8237bfdc8b915
Wes <Wes@DESKTOP-16T4F05>
2018-02-23 22:20:02 -0800

    Added Model

---
+++ ViaBikeRack/Models/BikeRackModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ViaBikeRack.Models
+{
+    public class BikeRackModel
+    {
+        public int Id { get; set; }
+
+        public int Route_Number { get; set; }
+
+        public int Bus_Number { get; set; }
+
+        public bool Bike_Rack_1 { get; set; }
+
+        public bool Bike_Rack_2 { get; set; }
+
+        public bool Bike_Rack_3 { get; set; }
+    }
+}
