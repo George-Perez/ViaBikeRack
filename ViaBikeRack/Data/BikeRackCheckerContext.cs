0a4f8a5cefd553ad32d7aafe456fbd68a020b5c6
Wes <Wes@DESKTOP-16T4F05>
2018-02-23 22:40:50 -0800

    Added Context

---
+++ ViaBikeRack/Data/BikeRackCheckerContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ViaBikeRack.Data
+{
+    public class BikeRackCheckerContext : DbContext
+    {
+        public BikeRackCheckerContext() : base("DefaultConnection")
+        {
+        }
+
+        public DbSet<BikeRackModel> BikeRackModels { get; set; }
+
+    }
+}
