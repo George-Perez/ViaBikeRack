345be291e88684dd25e8941b914040a05972a058
George-Perez <georgeperezis@gmail.com>
2018-02-24 00:08:55 -0600

    Add project files.

---
+++ ViaBikeRack/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ViaBikeRack.Startup))]
+namespace ViaBikeRack
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
