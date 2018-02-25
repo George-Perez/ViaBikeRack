f89c55d9d35e928336dcd2dbef760c00be4bbbd4
George-Perez <georgeperezis@gmail.com>
2018-02-24 22:12:17 -0600

    Added connection

--- ViaBikeRack/Migrations/ApplicationDbContext/Configuration.cs
+++ ViaBikeRack/Migrations/ApplicationDbContext/Configuration.cs
@@ -1,20 +1,19 @@
-namespace ViaBikeRack.Migrations
+namespace ViaBikeRack.Migrations.ApplicationDbContext
 {
     using System;
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
     using System.Linq;
 
-    internal sealed class Configuration : DbMigrationsConfiguration<ViaBikeRack.Data.BikeRackCheckerContext>
+    internal sealed class Configuration : DbMigrationsConfiguration<ViaBikeRack.Models.ApplicationDbContext>
     {
         public Configuration()
         {
             AutomaticMigrationsEnabled = false;
-            ContextKey = "ViaBikeRack.Data.BikeRackCheckerContext";
-            //ContextKey = "ViaBikeRack.Models.ApplicationDbContext";
+            MigrationsDirectory = @"Migrations\ApplicationDbContext";
         }
 
-        protected override void Seed(ViaBikeRack.Data.BikeRackCheckerContext context)
+        protected override void Seed(ViaBikeRack.Models.ApplicationDbContext context)
         {
             //  This method will be called after migrating to the latest version.
 
