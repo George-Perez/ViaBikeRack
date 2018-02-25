af6459a19ee0279158ea01f73b352f81610aaac2
George-Perez <georgeperezis@gmail.com>
2018-02-24 18:57:33 -0600

    added Wes changes

--- ViaBikeRack/Controllers/HomeController.cs
+++ ViaBikeRack/Controllers/HomeController.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ViaBikeRack.Data;
+using ViaBikeRack.Models;
 
 namespace ViaBikeRack.Controllers
 {
@@ -10,6 +13,14 @@ namespace ViaBikeRack.Controllers
     {
         public ActionResult Index()
         {
+            BikeRackModel bikeStatus = new BikeRackModel();
+            //Connecting to Database
+            BikeRackCheckerContext db = new BikeRackCheckerContext();
+
+            ViewBag.BusRoute = new SelectList(db.BikeRackModels, "Id", "Route_Number");
+
+            ViewBag.BusNumber = new SelectList(db.BikeRackModels, "Id", "Bus_Number");
+
             return View();
         }
 
@@ -22,9 +33,42 @@ namespace ViaBikeRack.Controllers
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
-
             return View();
         }
+
+        [HttpGet]
+        public ActionResult EditBikeRackState(int Id)
+        {
+            BikeRackModel bikeRack = new BikeRackModel();
+
+            return View("Index");
+        }
+
+        [HttpPost]
+        public ActionResult EditBikeRackState([Bind(Include = "Id,IsActive_Bike_Rack_1,IsActive_Bike_Rack_2,IsActive_Bike_Rack_3")] BikeRackModel bikeRackModel)
+        {
+            BikeRackCheckerContext db = new BikeRackCheckerContext();
+
+            BikeRackModel bikeRack = new BikeRackModel();
+
+            if (ModelState.IsValid)
+            {
+                var old_model = db.BikeRackModels.SingleOrDefault(x => x.Id == bikeRackModel.Id);
+                old_model = bikeRackModel;
+                //if (bikeRack.IsActive_Bike_Rack_1 == false)
+                //{
+                //    bikeRack.IsActive_Bike_Rack_1 = true;
+                //} else
+                //{
+                //    bikeRack.IsActive_Bike_Rack_1 = false;
+                //}
+
+                db.Entry(old_model).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(bikeRackModel);
+        }
+
     }
 }
---
+++ ViaBikeRack/Models/BusRouteViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.WebPages.Html;
+
+namespace ViaBikeRack.VIewModel
+{
+    public class BusRouteViewModel
+    {
+        [Display(Name = "Routes")]
+        public string SelectedBusRoute { get; set; }
+        public IEnumerable<SelectListItem> Routes { get; set; }
+    }
+}
